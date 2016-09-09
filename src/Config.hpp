010c211b84b05a6c5f1b2fa03d0d5e9d6cde2f1b
Hannes Haberl <hanneshaberlwsh@gmail.com>
2016-09-09 20:44:43 +0200

    add license notice to source files

--- src/Config.hpp
+++ src/Config.hpp
@@ -1,3 +1,23 @@
+/*
+ *	GLMViz is a OpenGL based Visualizer for mpd.
+ *	Copyright (C) 2016  Hannes Haberl
+ *	
+ *	This file is part of GLMViz.
+ *
+ *	GLMViz is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	GLMViz is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with GLMViz.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #pragma once
 
 #include <stdlib.h>
