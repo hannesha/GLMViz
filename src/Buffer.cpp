0e9e8093ca23ac77d538afd75df1426f355efc36
Hannes Haberl <hanneshaberlwsh@gmail.com>
2016-11-28 21:24:33 +0100

    Implement buffer write with offset

--- src/Buffer.cpp
+++ src/Buffer.cpp
@@ -48,6 +48,21 @@ void Buffer<T>::write(T buf[], const size_t n){
 }
 
 template<typename T>
+void Buffer<T>::write_offset(T buf[], const size_t n, const size_t gap, const size_t offset){
+	auto lock = this->lock();
+	new_data = true;
+
+	// limit data to write
+	size_t length = std::min(n, size);
+	size_t written = 0;
+	for(size_t i = offset; i<length; i = i+gap){
+		v_buffer.push_back(buf[i]);
+		written++;
+	}
+	v_buffer.erase(v_buffer.begin(), v_buffer.begin() + written);
+}
+
+template<typename T>
 void Buffer<T>::resize(const size_t n){
 	auto lock = this->lock();
 	size = n;
