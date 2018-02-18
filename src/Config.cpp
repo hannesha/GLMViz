2701f3c08d35e66a692a54c3d17bec80513a610e
Hannes Haberl <hanneshaberlwsh@gmail.com>
2018-02-18 23:36:53 +0100

    Fix fft normalization

--- src/Config.cpp
+++ src/Config.cpp
@@ -74,7 +74,7 @@ void Config::reload(){
 
 		// normalization value for the fft output
 		// calculate effective fft input data size
-		float isize = std::min(buf_size, fft.size)/2+1;
+		float isize = std::min(buf_size, fft.size);
 		fft.scale = 1.0f/(isize*32768.0f);
 
 		try{
